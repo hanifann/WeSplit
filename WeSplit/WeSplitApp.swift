83533ef722e520d8f9a3d36254cafc6cccb941ae
hanifan <hanifannurulhaq@outlook.com>
2024-03-08 16:10:08 +0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by hanifan nurul haq on 08/03/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
