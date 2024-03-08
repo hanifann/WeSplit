fcb4e519c890a0d2a76eba03df45340498355103
hanifan <hanifannurulhaq@outlook.com>
2024-03-08 17:33:34 +0700

    add tip percentage picker

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,11 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let tipPercentage = [10, 15, 20, 15, 0]
+    let tipPercentages = [0, 10, 15, 20]
     
     @State private var checkAmount = 0.0
     @State private var numberOfPeople = 2
-    @State private var tipPecentage = 0
+    @State private var tipPercentage = 0
     
     var body: some View {
         NavigationStack {
@@ -27,6 +27,16 @@ struct ContentView: View {
                             Text("\($0) people")
                         }
                     }
+                }
+                
+                Section("How much tip do you want to leave?") {
+                    
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
                 }
             }
             .navigationTitle("WeSplit")
